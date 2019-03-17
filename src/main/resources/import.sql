7abd73b8c6ca90d27173cf99d2ed1f7cae9708bb
Jump-Boy <Hi_JumpBoy@163.com>
2019-03-17 16:28:09 +0800

    提交Blogs项目文件

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO user (id, username, password, name, email) VALUES (1, 'admin', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi', '胡先森', 'hi_jumpboy@163.com');
+INSERT INTO user (id, username, password, name, email)  VALUES (2, 'humh', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi', 'humh', 'h18192681506@163.com');
+
+INSERT INTO authority (id, name) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO authority (id, name) VALUES (2, 'ROLE_USER');
+
+INSERT INTO user_authority (user_id, authority_id) VALUES (1, 1);
+INSERT INTO user_authority (user_id, authority_id) VALUES (2, 2);
